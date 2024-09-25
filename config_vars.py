bf900565cfe7fc7bd298402f277c6297babf3806
VicsRepository <melbvicduque@gmail.com>
2024-09-26 01:50:44 +1000

    Update config_vars.py
    Signed-off-by: VicsRepository <melbvicduque@gmail.com>

--- config_vars.py
+++ config_vars.py
@@ -1,4 +1,3 @@
-
 import os
 from dotenv import load_dotenv
 
@@ -6,4 +5,5 @@ load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-
+HEROKU_API_KEY = os.getenv('HEROKU_API_KEY')
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
